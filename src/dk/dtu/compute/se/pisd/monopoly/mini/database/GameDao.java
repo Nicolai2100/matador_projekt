3b5648c3edd5a69b00fae2d39b7427bb1718be4a
Jeppe <jeppe@kaarelarsen.dk>
2019-04-02 15:44:23 +0200

    Draft implementation of method: "loadGame". Added getListOfGames in IGameDAO.

--- src/dk/dtu/compute/se/pisd/monopoly/mini/database/GameDao.java
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/database/GameDao.java
@@ -2,17 +2,55 @@ package dk.dtu.compute.se.pisd.monopoly.mini.database;
 
 import dk.dtu.compute.se.pisd.monopoly.mini.model.Game;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 public class GameDao implements IGameDAO {
+
+    private Connection createConnection(){
+        return null;
+    }
+
     @Override
     public void saveGame(Game game) {
 
     }
 
+    /**
+     *
+     * @param gameId
+     * @return a game
+     * @author Jeppe s170196
+     */
     @Override
     public Game loadGame(int gameId) {
-        return null;
+        Game game = new Game();
+        try (Connection c = createConnection()) {
+
+            PreparedStatement gameStm = c.prepareStatement("SELECT * FROM Game WHERE GameID=?");
+            gameStm.setInt(1, gameId);
+
+            ResultSet gameRS = gameStm.executeQuery();
+
+
+            while (gameRS.next()) {
+                //TODO: Make resultset
+            }
+
+
+        } catch (SQLException e) {
+            //TODO: Handle exception?
+        }
+        return game;
     }
 
+    @Override
+    public List<Game> getGamesList() {
+        return null;
+    }
 
     @Override
     public void updateGame(Game game) {
--- src/dk/dtu/compute/se/pisd/monopoly/mini/database/IGameDAO.java
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/database/IGameDAO.java
@@ -2,10 +2,14 @@ package dk.dtu.compute.se.pisd.monopoly.mini.database;
 
 import dk.dtu.compute.se.pisd.monopoly.mini.model.Game;
 
+import java.util.List;
+
 public interface IGameDAO {
     void saveGame(Game game);
 
     Game loadGame(int gameId);
+
+    List<Game> getGamesList();
 
     void updateGame(Game game);
 
