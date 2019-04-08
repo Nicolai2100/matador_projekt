dfab282c1fe9b82c9d287ee8776a6f37ee2cdecd
Nicolai Larsen <nicolailarsen2100@gmail.com>
2019-04-08 15:28:50 +0200

    Ændret Main navn

--- src/dk/dtu/compute/se/pisd/monopoly/mini/Main.java
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/Main.java
@@ -1,22 +1,12 @@
 package dk.dtu.compute.se.pisd.monopoly.mini;
 
 import dk.dtu.compute.se.pisd.monopoly.mini.controller.GameController;
-import dk.dtu.compute.se.pisd.monopoly.mini.database.GameDAO;
 import dk.dtu.compute.se.pisd.monopoly.mini.model.*;
-import dk.dtu.compute.se.pisd.monopoly.mini.model.cards.CardMove;
-import dk.dtu.compute.se.pisd.monopoly.mini.model.cards.CardReceiveMoneyFromBank;
-import dk.dtu.compute.se.pisd.monopoly.mini.model.cards.PayTax;
-import dk.dtu.compute.se.pisd.monopoly.mini.model.properties.RealEstate;
-import dk.dtu.compute.se.pisd.monopoly.mini.model.properties.Utility;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Ekkart Kindler, ekki@dtu.dk
  */
-public class MiniMonopoly {
+public class Main {
     /**
      * The main method which creates a game, shuffles the chance
      * cards, creates players, and then starts the game. Note
