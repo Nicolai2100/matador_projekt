98ca526100af9b0fe2d47394c8ad12c01973c41d
wulff-dtu <s185036@student.dtu.dk>
2019-04-08 13:15:10 +0200

    Merget med branch Database...

--- src/dk/dtu/compute/se/pisd/monopoly/mini/controller/GameController.java
+++ src/dk/dtu/compute/se/pisd/monopoly/mini/controller/GameController.java
@@ -722,7 +722,7 @@ public class GameController {
         String accept = gui.getUserButtonPressed("Jeres byttehandel ser således ud:\n" + tradeOverview + "\nVil I acceptere og lukke handlen?", "Ja", "Nej");
 
         if (accept.equals("Ja")) {
-            //TODO: Lav færdig
+            //TODO: Lav det nu færdigt
         }
     }
 }
