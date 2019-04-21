6810a99d3142a914585441886440fc0cee6f161c
wulff-dtu <s185036@student.dtu.dk>
2019-04-21 17:29:53 +0200

    Flyttet implementeringen af chancekortene over i createData() i JSONutility og gjort så det virker.

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,14 +1,8 @@
 package game;
 
-import game.model.cards.CardMove;
-import game.model.cards.targetTypes;
 import json.JSONUtility;
 import game.controller.GameController;
-import game.model.Card;
 import game.model.Game;
-import game.model.cards.CardReceiveMoneyFromBank;
-
-import java.util.ArrayList;
 
 /**
  * @author Ekkart Kindler, ekki@dtu.dk
@@ -34,105 +28,6 @@ public class Main {
         temp.add(card);
         game.setCardDeck(temp);
         //*/
-
-
-        ArrayList<Card> cardList = new ArrayList<>();
-
-        Card card = new CardReceiveMoneyFromBank();
-        card.setText("Deres premieobligation er udtrukket. De modtager kr. 1000 af banken.");
-        ((CardReceiveMoneyFromBank) card).setAmount(1000);
-        cardList.add(card);
-
-        card = new CardReceiveMoneyFromBank();
-        card.setText("Værdien af egen avl fra nyttehaven udgør kr. 200, som De modtager af banken.");
-        ((CardReceiveMoneyFromBank) card).setAmount(200);
-        cardList.add(card);
-
-        //2
-        card = new CardReceiveMoneyFromBank();
-        card.setText("Grundet dyrtiden har De fået gageforhøjelse. Modtag kr. 1000.");
-        ((CardReceiveMoneyFromBank) card).setAmount(1000);
-        cardList.add(card);
-
-        //2
-        card = new CardReceiveMoneyFromBank();
-        card.setText("De modtager Deres aktieudbytte. Modtag kr. 1000 af banken.");
-        ((CardReceiveMoneyFromBank) card).setAmount(1000);
-        cardList.add(card);
-
-        card = new CardReceiveMoneyFromBank();
-        card.setText("De har solgt nogle gamle møbler på auktion. Modtag kr. 1000  af banken.");
-        ((CardReceiveMoneyFromBank) card).setAmount(1000);
-        cardList.add(card);
-
-        //2
-        card = new CardReceiveMoneyFromBank();
-        card.setText("De har vundet i Klasselotterliet. Modtag kr. 500.");
-        ((CardReceiveMoneyFromBank) card).setAmount(500);
-        cardList.add(card);
-
-        card = new CardReceiveMoneyFromBank();
-        card.setText("Kommunen har eftergivet et kvartals skat. Hæv i banken kr. 3000.");
-        ((CardReceiveMoneyFromBank) card).setAmount(3000);
-        cardList.add(card);
-
-        card = new CardReceiveMoneyFromBank();
-        card.setText("De har en række med elleve rigtige i tipning. Modtag kr. 1000.");
-        ((CardReceiveMoneyFromBank) card).setAmount(1000);
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk brikken frem til det nærmeste rederi og betal ejeren to gange den leje, han ellers er berettiget til." +
-                " Hvis selskabet ikke ejes af nogen, kan De købe det af banken.");
-        ((CardMove) card).setSpecialTarget(targetTypes.NEAREST_SHIP_1, game.getSpaces());
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Tag med den nærmeste færge. Flyt brikken frem, og hvis De passerer \"START\", indkassér da kr. 4000.");
-        ((CardMove) card).setSpecialTarget(targetTypes.NEAREST_SHIP_2, game.getSpaces());
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Gå i fængsel. Ryk direkte til fængslet. Selv om De passerer \"START\", indkasserer De ikke kr. 4000.");
-        ((CardMove) card).setTargetType(targetTypes.GO_TO_JAIL);
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk frem til " + game.getSpaces().get(32) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
-        ((CardMove) card).setTarget(game.getSpaces().get(32));
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk frem til " + game.getSpaces().get(19) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
-        ((CardMove) card).setTarget(game.getSpaces().get(19));
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk frem til " + game.getSpaces().get(24) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
-        ((CardMove) card).setTarget(game.getSpaces().get(24));
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Tag med " + game.getSpaces().get(15) + ". Flyt brikken frem, og hvis De passerer \"START\", indkassér da kr. 4000.");
-        ((CardMove) card).setTarget(game.getSpaces().get(15));
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk frem til \"START\".");
-        ((CardMove) card).setTarget(game.getSpaces().get(0));
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk tre felter frem.");
-        ((CardMove) card).setTargetType(targetTypes.THREE_FORWARDS);
-        cardList.add(card);
-
-        card = new CardMove();
-        card.setText("Ryk tre felter tilbage.");
-        ((CardMove) card).setTargetType(targetTypes.THREE_BACKWARDS);
-        cardList.add(card);
-
-        game.setCardDeck(cardList);
 
         GameController controller = new GameController(game);
         controller.playOrLoadGame();
--- src/json/JSONUtility.java
+++ src/json/JSONUtility.java
@@ -5,6 +5,9 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import game.model.*;
+import game.model.cards.CardMove;
+import game.model.cards.CardReceiveMoneyFromBank;
+import game.model.cards.targetTypes;
 import game.model.properties.Brewery;
 import game.model.properties.RealEstate;
 import game.model.properties.Ship;
@@ -12,6 +15,7 @@ import game.model.properties.Ship;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * Main-method creates data for JSON-file,
@@ -321,6 +325,103 @@ public class JSONUtility {
         raadhuspladsen.setPriceForHouse(4000);
         game.addSpace(raadhuspladsen);
 
+        ArrayList<Card> cardList = new ArrayList<>();
+
+        Card card = new CardReceiveMoneyFromBank();
+        card.setText("Deres premieobligation er udtrukket. De modtager kr. 1000 af banken.");
+        ((CardReceiveMoneyFromBank) card).setAmount(1000);
+        cardList.add(card);
+
+        card = new CardReceiveMoneyFromBank();
+        card.setText("Værdien af egen avl fra nyttehaven udgør kr. 200, som De modtager af banken.");
+        ((CardReceiveMoneyFromBank) card).setAmount(200);
+        cardList.add(card);
+
+        //2
+        card = new CardReceiveMoneyFromBank();
+        card.setText("Grundet dyrtiden har De fået gageforhøjelse. Modtag kr. 1000.");
+        ((CardReceiveMoneyFromBank) card).setAmount(1000);
+        cardList.add(card);
+
+        //2
+        card = new CardReceiveMoneyFromBank();
+        card.setText("De modtager Deres aktieudbytte. Modtag kr. 1000 af banken.");
+        ((CardReceiveMoneyFromBank) card).setAmount(1000);
+        cardList.add(card);
+
+        card = new CardReceiveMoneyFromBank();
+        card.setText("De har solgt nogle gamle møbler på auktion. Modtag kr. 1000  af banken.");
+        ((CardReceiveMoneyFromBank) card).setAmount(1000);
+        cardList.add(card);
+
+        //2
+        card = new CardReceiveMoneyFromBank();
+        card.setText("De har vundet i Klasselotterliet. Modtag kr. 500.");
+        ((CardReceiveMoneyFromBank) card).setAmount(500);
+        cardList.add(card);
+
+        card = new CardReceiveMoneyFromBank();
+        card.setText("Kommunen har eftergivet et kvartals skat. Hæv i banken kr. 3000.");
+        ((CardReceiveMoneyFromBank) card).setAmount(3000);
+        cardList.add(card);
+
+        card = new CardReceiveMoneyFromBank();
+        card.setText("De har en række med elleve rigtige i tipning. Modtag kr. 1000.");
+        ((CardReceiveMoneyFromBank) card).setAmount(1000);
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk brikken frem til det nærmeste rederi og betal ejeren to gange den leje, han ellers er berettiget til." +
+                " Hvis selskabet ikke ejes af nogen, kan De købe det af banken.");
+        ((CardMove) card).setSpecialTarget(targetTypes.NEAREST_SHIP_1, game.getSpaces());
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Tag med den nærmeste færge. Flyt brikken frem, og hvis De passerer \"START\", indkassér da kr. 4000.");
+        ((CardMove) card).setSpecialTarget(targetTypes.NEAREST_SHIP_2, game.getSpaces());
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Gå i fængsel. Ryk direkte til fængslet. Selv om De passerer \"START\", indkasserer De ikke kr. 4000.");
+        ((CardMove) card).setTargetType(targetTypes.GO_TO_JAIL);
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk frem til " + game.getSpaces().get(32) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
+        ((CardMove) card).setTarget(game.getSpaces().get(32));
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk frem til " + game.getSpaces().get(19) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
+        ((CardMove) card).setTarget(game.getSpaces().get(19));
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk frem til " + game.getSpaces().get(24) + ". Hvis De passerer \"START\", indkassér da kr. 4000.");
+        ((CardMove) card).setTarget(game.getSpaces().get(24));
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Tag med " + game.getSpaces().get(15) + ". Flyt brikken frem, og hvis De passerer \"START\", indkassér da kr. 4000.");
+        ((CardMove) card).setTarget(game.getSpaces().get(15));
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk frem til \"START\".");
+        ((CardMove) card).setTarget(game.getSpaces().get(0));
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk tre felter frem.");
+        ((CardMove) card).setTargetType(targetTypes.THREE_FORWARDS);
+        cardList.add(card);
+
+        card = new CardMove();
+        card.setText("Ryk tre felter tilbage.");
+        ((CardMove) card).setTargetType(targetTypes.THREE_BACKWARDS);
+        cardList.add(card);
+
+        game.setCardDeck(cardList);
 
         //JSON-GSON operations
         Path path = Paths.get("src/resources");
@@ -328,7 +429,8 @@ public class JSONUtility {
 
         FileWriter fileWriter = new FileWriter(path + "/game.json");
         GsonBuilder builder = new GsonBuilder().setPrettyPrinting()
-                .registerTypeAdapter(Space.class, new Adapter<Space>());
+                .registerTypeAdapter(Space.class, new Adapter<Space>())
+                .registerTypeAdapter(Card.class, new Adapter<Card>());
 
         Gson gson = builder.create();
 
@@ -368,7 +470,7 @@ public class JSONUtility {
         try {
             FileReader fileReader = new FileReader("src/resources/game.json");
 
-            GsonBuilder builder = new GsonBuilder().setPrettyPrinting().registerTypeAdapter(Space.class, new Adapter<Space>());
+            GsonBuilder builder = new GsonBuilder().setPrettyPrinting().registerTypeAdapter(Space.class, new Adapter<Space>()).registerTypeAdapter(Card.class, new Adapter<Card>());
 
             Gson gson = builder.create();
 
